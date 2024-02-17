2299f75cf6b48359c24c9796b2b10f88b43b957d
WangTangJang <1424435163@qq.com>
2024-02-17 23:06:39 +0800

    腰疼

--- src/main/test/demo/Demo.java
+++ src/main/test/demo/Demo.java
@@ -1,58 +1,27 @@
 package demo;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
-// 使用动态代理
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.InputStream;
+
 public class Demo {
+
+    @Autowired
+    private UserMapper userMapper;
+
     public static void main(String[] args) {
-        RealSubject realSubject = new RealSubject();
-        InvocationHandler handler = new MyInvocationHandler(realSubject);
-
-        // 创建动态代理对象
-        Subject proxy = (Subject) Proxy.newProxyInstance(
-                Subject.class.getClassLoader(),
-                new Class[] { Subject.class },
-                handler
-        );
-
-        // 通过代理调用真实对象的方法
-        proxy.request();
+        String resource = "mybatisContext.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = MybatisUtil
     }
 }
-
-
-// 接口
-interface Subject {
-    void request();
+interface UserMapper {
+    User selectUser(int id);
 }
 
-// 真实对象
-class RealSubject implements Subject {
-    public void request() {
-        System.out.println("RealSubject: Processing request.");
-    }
+class User {
+    private int id;
+    private String username;
+    private String password;
 }
-
-// InvocationHandler 实现
-class MyInvocationHandler implements InvocationHandler {
-    private Object realSubject;
-
-    public MyInvocationHandler(Object realSubject) {
-        this.realSubject = realSubject;
-    }
-
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        // 额外的操作
-        System.out.println("Proxy: Pre-processing request.");
-
-        // 调用真实对象的方法
-        Object result = method.invoke(realSubject, args);
-
-        // 额外的操作
-        System.out.println("Proxy: Post-processing request.");
-
-        return result;
-    }
-}
-
