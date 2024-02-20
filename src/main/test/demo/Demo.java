03fc4df5281dad2429e864ae4775c3c77d3720b2
WangTangJang <1424435163@qq.com>
2024-02-20 23:45:57 +0800

    好点子

--- src/main/test/demo/Demo.java
+++ src/main/test/demo/Demo.java
@@ -1,25 +1,24 @@
 package demo;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.InputStream;
+import java.io.IOException;
 
 public class Demo {
-
-    @Autowired
-    private UserMapper userMapper;
-
-    public static void main(String[] args) {
-        String resource = "mybatisContext.xml";
+    public static void main(String[] args) throws IOException {
+        System.out.println("请输入ip地址：");
+        byte[] bytes = new byte[1024];
+        int len = System.in.read(bytes);
+        String str = new String(bytes, 0, len - 1);
+        //    将接收的ip地址转为二进制
+        String[] split = str.split("\\.");
+        StringBuilder sb = new StringBuilder();
+        for (String value : split) {
+            StringBuilder s = new StringBuilder(Integer.toBinaryString(Integer.parseInt(value)));
+            while (s.length() < 8) {
+                s.insert(0, "0");
+            }
+            sb.append(s).append(".");
+        }
+        System.out.println(sb.toString());
     }
-}
-interface UserMapper {
-    User selectUser(int id);
-}
-
-class User {
-    private int id;
-    private String username;
-    private String password;
 }
